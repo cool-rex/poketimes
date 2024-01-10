590105640e0a614ddaeab32fe2bee8d4c35216c1
cool-rex <rbrefo2@gmail.com>
2024-01-10 17:07:07 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+          <Routes>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="/contact" Component={Contact}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+    }
+  
+  }
+ 
 export default App;
