1e8c7a3f3719619817e45a2933748d999187c6a3
cool-rex <rbrefo2@gmail.com>
2024-01-16 15:19:56 +0000

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -6,13 +7,13 @@ const Navbar = () => {
       <div className="container">
         <a href="/" className="brand-logo">Poke'Times</a>
         <ul className="right">
-            <li> <a href="/">Home</a></li>
-            <li> <a href="/about">About</a></li>
-            <li> <a href="/contact">Contact</a></li>
+            <li> <Link to="/">Home</Link></li>
+            <li> <NavLink to="/about">About</NavLink></li>
+            <li> <Link to="/contact">Contact</Link></li>
         </ul>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Navbar
