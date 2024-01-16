ccee99a9250b171b366489cba604a8077df871d2
cool-rex <rbrefo2@gmail.com>
2024-01-16 17:34:12 +0000

    update

---
+++ src/Components/hoc/Rianbow.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Rainbow = (wrappedComponent)=>{
+
+    const colours = ['red','pink','orange','blue','green', 'yellow']
+    const randomColour= colours[Math.floor(Math.random() * 5)]
+    const className = randomColour + "-text";
+
+
+    return(props) => {
+        return(
+            <div className={className}>
+                <wrappedComponent/>
+            </div>
+        )
+    }
+
+
+}
+
