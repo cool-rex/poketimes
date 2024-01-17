e06a10af418843f680dbcf589da243ba82cadc11
cool-rex <rbrefo2@gmail.com>
2024-01-17 14:11:49 +0000

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,10 @@
 import React  from 'react';
 import Rainbow from './hoc/Rianbow';
 
-const About =()=>{
+const About =(props)=>{
     return(
        <div className='container'>
-        <h4 className='center'>About</h4>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        
        </div>
     )
 }
--- src/Components/hoc/Rianbow.js
+++ src/Components/hoc/Rianbow.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Rainbow = (wrappedComponent)=>{
 
-    const colours = ['red','pink','orange','blue','green', 'yellow']
-    const randomColour= colours[Math.floor(Math.random() * 5)]
+    const colours = ['red','pink','orange','blue','green', 'yellow'];
+    const randomColour= colours[Math.floor(Math.random() * 5)];
     const className = randomColour + "-text";
 
 
@@ -11,6 +11,10 @@ const Rainbow = (wrappedComponent)=>{
         return(
             <div className={className}>
                 <wrappedComponent {...props}/>
+                <div className='container'>
+        <h4 className='center'>About</h4>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+       </div>
             </div>
         )
     }
